b29279337db3a064e5082825f10449958df732df
00012467 <wiut12467@gmail.com>
2023-10-29 22:30:00 +0500

    stil....

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script runs after the new version of your application is installed.
-chown -R ec2-user:ec2-user /var/www/games-api
+chown -R ec2-user:ec2-user /home/ec2-user/games-api
