b4a16180aba57475e3c13aa047fd680502cea056
00012467 <wiut12467@gmail.com>
2023-10-29 22:28:25 +0500

    Still trying

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script runs before the old version of your application is stopped.
+cd /var/www/games-api/DSCC_API
 pkill -f "dotnet DSCC_API.dll"
--- scripts/before-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the source directory
-cd /var/www/games-api/
-
-# Copy appsettings.json to the destination
-cp appsettings.json /path/to/destination/appsettings.json
