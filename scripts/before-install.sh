4ca29d0fc9c1d4935435a17d447e0af0b34b8564
00012467 <wiut12467@gmail.com>
2023-10-29 22:05:37 +0500

    Changed script again

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# This script runs before the new version of your application is installed.
-cp /var/www/games-api/DSCC_API/appsettings.json
+
+# Navigate to the source directory
+cd /var/www/games-api/DSCC_API
+
+# Copy appsettings.json to the destination
+cp appsettings.json /path/to/destination/
