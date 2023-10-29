b1a3e9c818e700beabf6e475b55bca351a19c7d0
00012467 <wiut12467@gmail.com>
2023-10-29 22:18:51 +0500

    Change in the files. still trying to deploy

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script runs after the new version of your application is successfully installed and started.
-cd /var/www/games-api/publish
+cd /var/www/games-api/DSCC_API
 dotnet DSCC_API.dll
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the source directory
-cd /var/www/games-api/DSCC_API
+cd /var/www/games-api/
 
 # Copy appsettings.json to the destination
-cp appsettings.json /path/to/destination/
+cp appsettings.json /path/to/destination/appsettings.json
